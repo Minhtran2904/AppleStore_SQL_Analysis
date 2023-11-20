ee37ef344c4b8aebb6d2c348dff9605545b2c772
Minhtran2904 <anhminh.2904@gmail.com>
2023-11-20 09:32:42 +0200

    Perform unique check and missing value check

--- MyScript.sql
+++ MyScript.sql
@@ -14,3 +14,14 @@ UNION ALL
 
 SELECT * FROM appleStore_description4
 
+/* Perform unique check to assure both dataset is the same, for future joint and analysis*/AppleStore
+SELECT COUNT(DISTINCT id) AS UniqueAppIDs FROM AppleStore
+
+SELECT COUNT(DISTINCT id) AS UniqueAppIDs FROM appleStore_description_combined
+
+/*Check for any missing values in key fields*/
+SELECT COUNT(*) AS MissingValues FROM AppleStore 
+WHERE track_name IS NULL OR user_rating IS NULL OR prime_genre IS NULL
+
+SELECT COUNT(*) AS MissingValues FROM appleStore_description_combined
+WHERE app_desc IS NULL
