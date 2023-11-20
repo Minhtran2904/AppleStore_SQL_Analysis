c0ebde4ab5650b19867bbcf8cf2fbbf8e1623a94
Minhtran2904 <anhminh.2904@gmail.com>
2023-11-20 09:31:40 +0200

    Adding SQL script

---
+++ MyScript.sql
@@ -0,0 +1,16 @@
+CREATE TABLE appleStore_description_combined AS
+
+SELECT * FROM appleStore_description1
+
+UNION ALL
+
+SELECT * FROM appleStore_description2
+
+UNION ALL
+
+SELECT * FROM appleStore_description3
+
+UNION ALL 
+
+SELECT * FROM appleStore_description4
+
